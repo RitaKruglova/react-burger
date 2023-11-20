266902e8ae76f012df592d6083a6188b5b336017
Rita Kruglova <kruglova404@mail.ru>
2023-11-20 16:25:26 +0300

    feat: add BurgerNavigation comonent

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -1,10 +1,13 @@
 import burgerIngredientsStyles from './burger-ingredients.module.css';
+import BurgerNavigation from './burger-navigation/burger-navigation';
 
 function BurgerIngredients() {
+  
+
   return (
     <section className={burgerIngredientsStyles.container}>
       <h1 className={burgerIngredientsStyles.title}>Соберите бургер</h1>
-      
+      <BurgerNavigation />
     </section>
   )
 }
--- src/components/burger-ingredients/burger-navigation/burger-navigation.js
+++ src/components/burger-ingredients/burger-navigation/burger-navigation.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import burgerNavigationStyles from './burger-navigation.module.css';
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 
 function BurgerNavigation() {
+  const [current, setCurrent] = useState('булки');
+
   return (
     <nav className={burgerNavigationStyles.menu}>
-      
+      <Tab value="булки" active={current === "булки"} onClick={setCurrent}>
+        Булки
+      </Tab>
+      <Tab value="соусы" active={current === "соусы"} onClick={setCurrent}>
+        Соусы
+      </Tab>
+      <Tab value="начинки" active={current === "начинки"} onClick={setCurrent}>
+        Начинки
+      </Tab>
     </nav>
   )
 }
+
+export default BurgerNavigation;
