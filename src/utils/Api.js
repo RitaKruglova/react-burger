a2db1c0a1d340dac73e0a9bc9929fd7b4128d954
Rita Kruglova <kruglova404@mail.ru>
2023-11-24 12:58:00 +0300

    feat: add class Api

---
+++ src/utils/Api.js
@@ -0,0 +1,33 @@
+import { API_URL } from "../constants/constants";
+import { checkResponse } from "./check-response";
+
+class Api {
+  constructor({ url, headers }) {
+    this._url = url;
+    this._headers = headers;
+  }
+
+  _fetch(url = '', method = 'GET', body = null) {
+    return fetch(`${this._url}${url}`, {
+      method,
+      headers: this._headers,
+      body,
+      mode: 'cors',
+      credentials: 'include'
+    })
+      .then(checkResponse)
+  }
+
+  getIngredients() {
+    return this._fetch();
+  }
+}
+
+const api = new Api({
+  url: API_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+export { api }
