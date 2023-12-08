2ddd5059c944835ada61ca4e1ac14116b6b3e73c
Rita Kruglova <kruglova404@mail.ru>
2023-12-08 20:24:11 +0300

    fix: remove positionSlice

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,14 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import ingredientsReducer from './slices/ingredientsSlice.js';
 import tabsReducer from './slices/tabsSlice.js';
 import orderReducer from './slices/orderSlice.js';
-import positionsReducer from './slices/positionsSlice.js';
 
 const store = configureStore({
   reducer: {
     ingredients: ingredientsReducer,
     tabs: tabsReducer,
     order: orderReducer,
-    positions: positionsReducer
   },
 })
 
