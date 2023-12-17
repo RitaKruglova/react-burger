4bf744d40d4dd0bf7b4e96f9da27a3541dc00b49
Rita Kruglova <kruglova404@mail.ru>
2023-12-17 14:53:21 +0300

    feat: add HomePage route

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,22 +2,12 @@ import { useEffect } from 'react';
 import appStyles from './app.module.css';
 import Header from '../header/header';
 import { Route, Routes } from 'react-router-dom';
-import Burger from '../burger/burger';
-import Preloader from '../preloader/preloader';
-import BurgerIngredients from '../burger-ingredients/burger-ingredients';
-import BurgerConstructor from '../burger-constructor/burger-constructor';
 import { fetchIngredients } from '../../store/slices/ingredientsSlice';
 import { useDispatch, useSelector } from 'react-redux';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-
-// Уважаемый господин Ревьюер, подскажите пожалуйста как мне исправить warning из консоли и где эта ошибка вообще находится, я не поняла(((. Заранее спасибо
+import HomePage from '../../pages/home-page';
 
 function App() {
-  const { isLoading, error } = useSelector(store => ({
-    isLoading: store.loading.isLoading,
-    error: store.ingredients.error
-  }));
+  const error = useSelector(store => store.ingredients.error);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -34,15 +24,7 @@ function App() {
         <Routes>
           <Route
             path="/"
-            element={!isLoading ?
-              <Burger>
-                <DndProvider backend={HTML5Backend}>
-                  <BurgerIngredients />
-                  <BurgerConstructor />
-                </DndProvider>
-              </Burger>
-            :
-            <Preloader />}
+            element={<HomePage />}
           />
         </Routes>
       </main>
---
+++ src/pages/home-page.js
@@ -0,0 +1,27 @@
+import Burger from '../components/burger/burger';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import BurgerIngredients from '../components/burger-ingredients/burger-ingredients';
+import BurgerConstructor from '../components/burger-constructor/burger-constructor';
+import Preloader from '../components/preloader/preloader';
+import { useSelector } from 'react-redux';
+
+function HomePage() {
+  const isLoading = useSelector(store => store.loading.isLoading);
+
+  return (
+    <>
+      {!isLoading ?
+        <Burger>
+          <DndProvider backend={HTML5Backend}>
+            <BurgerIngredients />
+            <BurgerConstructor />
+          </DndProvider>
+        </Burger>
+      :
+      <Preloader />}
+    </>
+  )
+}
+
+export default HomePage;
