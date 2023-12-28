9dbdc1d8f711212ab88090dfaaa79c5de7530efd
Rita Kruglova <kruglova404@mail.ru>
2023-12-28 15:08:36 +0300

    fix: correct active links in header

--- src/components/header/menu-item/menu-item.js
+++ src/components/header/menu-item/menu-item.js
@@ -1,33 +1,32 @@
 import menuItemStyles from './menu-item.module.css';
-import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import { ListIcon } from '@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons';
-import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons';
-import { useEffect, useState } from 'react';
+import { BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { useMemo } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function MenuItem({ isBurgerConstructor, text, isInContainer, path }) {
   const location = useLocation();
 
-  const [isActive, setIsActive] = useState(location.pathname === path);
-
-  useEffect(() => {
-    setIsActive(location.pathname === path);
+  const isLinkActive = useMemo(() => {
+    if (path === '/') {
+      return location.pathname === path;
+    }
+    return location.pathname.startsWith(path);
   }, [location.pathname, path]);
 
   return (
     <NavLink
       to={path}
-      className={`${menuItemStyles.link} ${!isInContainer ? menuItemStyles.right : ''} mr-2 pr-5`}
+      className={({ isActive }) => 
+        `${menuItemStyles.link} ${!isInContainer ? menuItemStyles.right : ''} mr-2 pr-5 ${isActive ? menuItemStyles.active : ''}`
+      }
     >
-      {isBurgerConstructor && <BurgerIcon type={isActive ? 'primary' : 'secondary'} />}
-      {!isBurgerConstructor && isInContainer && <ListIcon type={isActive ? 'primary' : 'secondary'} />}
-      {!isInContainer && <ProfileIcon type={isActive ? 'primary' : 'secondary'} />}
-      <p
-        className={`${menuItemStyles.name} ${isActive ? menuItemStyles.white : ''} ml-2 text text_type_main-default`}>{text}
-      </p>
+      {isBurgerConstructor && <BurgerIcon type={isLinkActive ? 'primary' : 'secondary'} />}
+      {!isBurgerConstructor && isInContainer && <ListIcon type={isLinkActive ? 'primary' : 'secondary'} />}
+      {!isInContainer && <ProfileIcon type={isLinkActive ? 'primary' : 'secondary'} />}
+      <p className={`${menuItemStyles.name} ${isLinkActive ? menuItemStyles.white : ''} ml-2 text text_type_main-default`}>{text}</p>
     </NavLink>
-  )
+  );
 }
 
 MenuItem.propTypes = {
@@ -35,6 +34,6 @@ MenuItem.propTypes = {
   text: PropTypes.string.isRequired,
   isInContainer: PropTypes.bool.isRequired,
   path: PropTypes.string.isRequired
-}
+};
 
 export default MenuItem;
