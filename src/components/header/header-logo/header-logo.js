e578537a41b435ad77c1dd05c470e5b1328487ed
Rita Kruglova <kruglova404@mail.ru>
2023-12-28 15:11:15 +0300

    feat: add link to logo

--- src/components/header/header-logo/header-logo.js
+++ src/components/header/header-logo/header-logo.js
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import headerLogoStyles from './header-logo.module.css';
 import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
 
 function HeaderLogo() {
   return (
     <div className={headerLogoStyles.container}>
-      <Logo />
+      <Link to="/">
+        <Logo />
+      </Link>
     </div>
   )
 }
