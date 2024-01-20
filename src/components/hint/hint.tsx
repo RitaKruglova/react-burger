ae9c03a05c66b8244034bdc8e17ea62fbceb66b8
Rita Kruglova <kruglova404@mail.ru>
2024-01-20 13:00:59 +0300

    feat: get hint to tsx

--- src/components/hint/hint.tsx
+++ src/components/hint/hint.tsx
@@ -1,21 +1,21 @@
 import { Link } from 'react-router-dom';
 import hintStyles from './hint.module.css';
-import PropTypes from 'prop-types';
+import { FC } from 'react';
 
-function Hint({ paragraphText, linkPath, linkText, needIndent }) {
+interface IHint {
+  paragraphText: string;
+  linkPath: string;
+  linkText: string;
+  needIndent: boolean;
+}
+
+const Hint: FC<IHint> = ({ paragraphText, linkPath, linkText, needIndent }) => {
   return (
     <div className={`${hintStyles.container}${needIndent ? " mb-4" : ""}`}>
       <p className={`${hintStyles.text} text text_type_main-default mr-2`}>{paragraphText}</p>
       <Link to={linkPath} className={`${hintStyles.link} text text_type_main-default`}>{linkText}</Link>
     </div>
   )
-}
-
-Hint.propTypes = {
-  paragraphText: PropTypes.string.isRequired,
-  linkPath: PropTypes.string.isRequired,
-  linkText: PropTypes.string.isRequired,
-  needIndent: PropTypes.bool.isRequired
 }
 
 export default Hint;
