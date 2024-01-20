6d349d77abb887c2a6a6fb196a67b3a9e2cc4603
Rita Kruglova <kruglova404@mail.ru>
2024-01-20 12:18:06 +0300

    feat: get protected-route to jsx

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,8 +1,14 @@
 import { useAppSelector } from "../../utils/reduxHooks";
 import { Navigate, useLocation } from "react-router-dom";
 import { loginRoute } from "../../constants/constants";
+import { ComponentType, FC } from "react";
 
-function ProtectedRoute({element: Component, ...props}) {
+interface IProtectedRouteProps {
+  element: ComponentType<any>;
+  [key: string]: any;
+}
+
+const ProtectedRoute: FC<IProtectedRouteProps> = ({element: Component, ...props}) => {
   const currentUser = useAppSelector(store => store.form.currentUser);
 
   const location = useLocation();
