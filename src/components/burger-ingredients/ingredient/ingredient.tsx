b0e4a4ce6cec617eacce211f1a4e054280c2ac41
Rita Kruglova <kruglova404@mail.ru>
2024-01-20 14:57:16 +0300

    feat: get ingredient to tsx

--- src/components/burger-ingredients/ingredient/ingredient.tsx
+++ src/components/burger-ingredients/ingredient/ingredient.tsx
@@ -1,10 +1,15 @@
 import ingredientStyles from './ingredient.module.css';
 import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
-import PropTypes from 'prop-types';
 import { useDrag } from 'react-dnd/dist/hooks';
-import { ingredientType } from '../../../utils/types';
+import { TIngredient } from '../../../utils/types';
+import { FC } from 'react';
 
-function Ingredient({ ingredient, onClick }) {
+interface IIngredient {
+  ingredient: TIngredient;
+  onClick: () => void;
+}
+
+const Ingredient: FC<IIngredient> = ({ ingredient, onClick }) => {
   const [, dragRef] = useDrag({
     type: 'ingredient',
     item: ingredient
@@ -18,16 +23,11 @@ function Ingredient({ ingredient, onClick }) {
         <CurrencyIcon type="primary" />
       </div>
       <h3 className={`${ingredientStyles.name} text text_type_main-default`}>{ingredient['name']}</h3>
-      {ingredient.count > 0 &&
+      {ingredient.count && ingredient.count > 0 &&
         <Counter count={ingredient.count} size="default" extraClass="m-1" />
       }
     </li>
   )
-}
-
-Ingredient.propTypes = {
-  ingredient: ingredientType.isRequired,
-  onClick: PropTypes.func.isRequired
 }
 
 export default Ingredient;
