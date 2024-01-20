f4d38cd51e9cb1b695a58348d1a2e7dba41ae1c0
Rita Kruglova <kruglova404@mail.ru>
2024-01-20 13:06:30 +0300

    feat: get header-logo to tsx

--- src/components/header/header-logo/header-logo.tsx
+++ src/components/header/header-logo/header-logo.tsx
@@ -2,8 +2,9 @@ import { Link } from 'react-router-dom';
 import headerLogoStyles from './header-logo.module.css';
 import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
 import { mainRoute } from '../../../constants/constants';
+import { FC } from 'react';
 
-function HeaderLogo() {
+const HeaderLogo: FC = () => {
   return (
     <div className={headerLogoStyles.container}>
       <Link to={mainRoute}>
