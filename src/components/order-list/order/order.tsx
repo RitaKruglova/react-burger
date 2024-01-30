01d76e2abefb333d57c8c3a2e2e6ec8c749ffa66
Rita Kruglova <kruglova404@mail.ru>
2024-01-30 19:41:14 +0300

    feat: add styles for order component

--- src/components/order-list/order/order.tsx
+++ src/components/order-list/order/order.tsx
@@ -10,25 +10,33 @@ const Order: FC = () => {
 
 
   return (
-    <li className={`${orderStyles.container} p-6`}>
+    <li className={`${orderStyles.container} p-6 mb-4`}>
       <div className={`${orderStyles.info} mb-6`}>
         <p className="text text_type_digits-default">#034535</p>
-        <p className="text text_type_main-default">Сегодня, 16:20</p>
+        <p className="text text_type_main-default text_color_inactive">Сегодня, 16:20</p>
       </div>
       <h3 className="text text_type_main-medium mb-6">Death Star Starship Main бургер</h3>
       <div className={orderStyles.order}>
         <div className={`${orderStyles.ingredients} mr-6`}>
-          {dataIngredients.slice(0, 5).map(i => (
-            <img key={i._id} className={orderStyles.image} src={i.image} alt={i.name} />
+          {dataIngredients.slice(0, 5).map((i, index) => (
+            <img
+              key={i._id}
+              className={orderStyles.image}
+              src={i.image}
+              alt={i.name}
+              style={{zIndex: 5 - index}}
+            />
           ))}
           {dataIngredients.length > 5 &&
             <div className={orderStyles.extra} style={{ backgroundImage: `url(${dataIngredients[5].image})` }}>
-              +{dataIngredients.length - 5}
+              <div className={`${orderStyles.text} text text_type_digits-default`}>
+                +{dataIngredients.length - 5}
+              </div>
             </div>
           }
         </div>
         <div className={orderStyles.price}>
-          <p className="text text_type_digits-medium">480</p>
+          <p className="text text_type_digits-default mr-2">480</p>
           <CurrencyIcon type="primary"/>
         </div>
       </div>
--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -5,7 +5,7 @@ import OrderList from '../components/order-list/order-list';
 const Feed: FC = () => {
   return (
     <section className={feedStyles.section}>
-      <h2 className="text text_type_main-large">Лента заказов</h2>
+      <h2 className="text text_type_main-large mt-10 mb-5">Лента заказов</h2>
       <div className={feedStyles.container}>
         <OrderList />
       </div>
